e6fdad03366c880bc46d1ac3a92742bd7da3aa52
Ron Hockman <ron@hockman.org>
2019-11-29 09:47:52 -0500

    Get rid of references to Hugo, since I no longer use it.
    Take in a root_path parameter during build to handle non-root paths

--- html_builder/__main__.py
+++ html_builder/__main__.py
@@ -1,5 +1,3 @@
-import datetime
-import json
 import sqlite3
 
 import os
@@ -8,73 +6,20 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 from slugify import slugify
 
 
-class HugoBuilder:
+class HtmlBuilder:
     def __init__(self):
         pass
 
-    # noinspection SqlWithoutWhere
-    def import_json(self, paints):
-        with open(paints, "r") as fin:
-            paints_json = json.load(fin)
-
-        db = sqlite3.connect("pigments.sqlite3")
-
-        cursor = db.cursor()
-        cursor.execute('''DELETE FROM manufacturers''')
-        cursor.execute('''DELETE FROM sqlite_sequence where name = 'manufacturers' ''')
-        cursor.execute('''DELETE FROM paints''')
-        cursor.execute('''DELETE FROM sqlite_sequence where name = 'paints' ''')
-        cursor.execute('''DELETE FROM pigments''')
-        cursor.execute('''DELETE FROM sqlite_sequence where name = 'pigments' ''')
-        cursor.execute('''DELETE FROM pigments_to_paint''')
-        cursor.execute('''DELETE FROM sqlite_sequence where name = 'pigments_to_paint' ''')
-        db.commit()
-
-        for manufacturer in paints_json.keys():
-            execute_result = cursor.execute('''INSERT INTO manufacturers(name) values (?)''', (manufacturer,))
-
-            manufacturer_id = execute_result.lastrowid
-
-            for paint_name, pigments in paints_json[manufacturer][0].items():
-                cursor.execute('''insert into paints(manufacturer_id, name) VALUES (?,?)''',
-                               (manufacturer_id, paint_name))
-                paint_id = cursor.lastrowid
-
-                for pigment_detail in pigments:
-                    pigment_code, pigment_name = pigment_detail
-
-                    cursor.execute('''select id from pigments where pigments.code=? and pigments.name=? ''',
-                                   (pigment_code, pigment_name))
-                    pigment_result = cursor.fetchone()
-
-                    if pigment_result:
-                        pigment_id = pigment_result[0]
-                        cursor.execute('''insert into pigments_to_paint(paint_id, pigment_id) values(?, ?)''',
-                                       (paint_id, pigment_id))
-                    else:
-                        cursor.execute('''insert into pigments(code, name) VALUES (?, ?)''',
-                                       (pigment_code, pigment_name))
-                        pigment_id = cursor.lastrowid
-                        cursor.execute('''insert into pigments_to_paint(paint_id, pigment_id) values(?, ?)''',
-                                       (paint_id, pigment_id))
-
-            db.commit()
-
-        db.commit()
-        db.close()
-
-    def build(self):
+    def build(self, root_path = ''):
         shutil.rmtree("build", ignore_errors=True)
-        shutil.copytree(os.path.join("hugo_builder", "css"), os.path.join("build"))
+        shutil.copytree(os.path.join("html_builder", "css"), os.path.join("build"))
 
         os.makedirs("build", exist_ok=True)
         os.makedirs(os.path.join("build", "paints"), exist_ok=True)
         os.makedirs(os.path.join("build", "pigments"), exist_ok=True)
 
-        render_date = datetime.datetime.now()
-
         env = Environment(
-            loader=PackageLoader('hugo_builder', 'templates'),
+            loader=PackageLoader('html_builder', 'templates'),
             autoescape=select_autoescape(['html', 'xml', 'md'])
         )
 
@@ -82,30 +27,51 @@ class HugoBuilder:
         pigment_template = env.get_template('pigment.html')
         manufacturer_index_template = env.get_template('man_index.html')
         paint_index_template = env.get_template('paint_index.html')
+        pigment_index_template = env.get_template('pigment_index.html')
 
         db = sqlite3.connect("pigments.sqlite3")
         cursor = db.cursor()
 
-        manufacturers_result = cursor.execute('''select id, name from manufacturers order by name''').fetchall()
+        # Generate Nav bar variables
+        nav_pigments_result = cursor.execute('''select code, name from pigments order by code, name''').fetchall()
+        nav_pigments = [(pigment_code, pigment_name, slugify(pigment_code), slugify(pigment_name)) for
+                        (pigment_code, pigment_name) in nav_pigments_result]
+
+        # Generate Pigment Index
+        pigment_result = pigment_index_template.render(nav_pigments=nav_pigments, root_path=root_path)
+        with open(os.path.join("build", "pigments", "index.html"), "w+") as fout:
+            fout.writelines(pigment_result)
+
+        manufacturers_result = cursor.execute('''select manufacturers.id, manufacturers.name, count() 
+        from manufacturers 
+        inner join paints p on manufacturers.id = p.manufacturer_id 
+        group by manufacturers.id, manufacturers.name
+        order by manufacturers.name''').fetchall()
         manufacturers = []
 
-        for (manufacturer_id, manufacturer) in manufacturers_result:
+        for (manufacturer_id, manufacturer, paint_count) in manufacturers_result:
             man_slug = slugify(manufacturer)
             os.makedirs(os.path.join("build", "paints", man_slug), exist_ok=True)
-            manufacturers.append([manufacturer, man_slug])
+            manufacturers.append([manufacturer, man_slug, paint_count])
 
             paints = []
 
-            paint_results = cursor.execute('''select name from paints where manufacturer_id=? order by name''',
+            paint_results = cursor.execute('''
+            select p.name, count()
+            from paints as p 
+            inner join pigments_to_paint ptp on p.id = ptp.paint_id
+            where p.manufacturer_id=? 
+            group by p.name
+            order by name''',
                                            (manufacturer_id,)).fetchall()
 
-            for (paint_name,) in paint_results:
-                paints.append([paint_name, slugify(paint_name)])
-            paint_result = paint_index_template.render(manufacturer=manufacturer, paints=paints, man_slug=man_slug)
+            for (paint_name, pigment_count) in paint_results:
+                paints.append([paint_name, slugify(paint_name), pigment_count])
+            paint_result = paint_index_template.render(manufacturer=manufacturer, paints=paints, man_slug=man_slug, root_path=root_path)
             with open(os.path.join("build", "paints", man_slug, "index.html"), "w+") as fout:
                 fout.writelines(paint_result)
 
-        man_index = manufacturer_index_template.render(manufacturers=manufacturers)
+        man_index = manufacturer_index_template.render(manufacturers=manufacturers, root_path=root_path)
         with open(os.path.join("build", "index.html"), "w+") as fout:
             fout.writelines(man_index)
 
@@ -130,12 +96,14 @@ class HugoBuilder:
                 pigments.append([pigment_code, pigment_name, pigment_url])
 
             result = paint_template.render(manufacturer=manufacturer_name,
-                                           pigments=pigments, paint_name=name, man_slug=man_slug)
+                                           pigments=pigments, paint_name=name, man_slug=man_slug, root_path=root_path)
             with open(os.path.join("build", "paints", man_slug, paint_file), "w+") as fout:
                 fout.writelines(result)
 
-        pigments_result = cursor.execute('''select id, code, name from pigments order by code, name''').fetchall()
-        for (pigment_id, pigment_code, pigment_name) in pigments_result:
+        # Pigment Info Page
+        pigments_result = cursor.execute(
+            '''select id, code, name, description from pigments order by code, name''').fetchall()
+        for (pigment_id, pigment_code, pigment_name, pigment_description) in pigments_result:
             pigment_url = "{}-{}.html".format(slugify(pigment_code), slugify(pigment_name))
             single = []
             multiple = []
@@ -160,15 +128,23 @@ class HugoBuilder:
 
             for (manufacturer_name, paint_name, paint_count) in single_paint_summary:
                 single.append(
-                    [manufacturer_name + " - " + paint_name, "/paints/{}/{}.html".format(slugify(manufacturer_name), slugify(paint_name))])
+                    [manufacturer_name + " - " + paint_name,
+                     "{}/paints/{}/{}.html".format(root_path, slugify(manufacturer_name), slugify(paint_name))])
 
             for (manufacturer_name, paint_name, pigment_count) in multiple_paint_summary:
                 multiple.append(
-                    [manufacturer_name + " - " + paint_name, "/paints/{}/{}.html".format(slugify(manufacturer_name), slugify(paint_name)),
+                    [manufacturer_name + " - " + paint_name,
+                     "{}/paints/{}/{}.html".format(root_path, slugify(manufacturer_name), slugify(paint_name)),
                      pigment_count])
 
+            pigment_link_result = cursor.execute('''select source, url from pigment_links where pigment_id=?''',
+                                                 (pigment_id,)).fetchall()
+
             result = pigment_template.render(pigment_code=pigment_code, pigment_name=pigment_name, single=single,
-                                             multiple=multiple)
+                                             multiple=multiple, pigment_description=pigment_description,
+                                             pigment_links=pigment_link_result,
+                                             root_path=root_path
+                                             )
 
             with open(os.path.join("build", "pigments", pigment_url), "w+") as fout:
                 fout.writelines(result)
@@ -177,5 +153,5 @@ class HugoBuilder:
 
 
 if __name__ == "__main__":
-    hb = HugoBuilder()
-    hb.build()
+    hb = HtmlBuilder()
+    hb.build(root_path='/pigments')
